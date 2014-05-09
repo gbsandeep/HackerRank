54f047080a8f41ad873933d3c01f1a420838f140
gbsandeep <gbsandeep@gmail.com>
2014-05-09 13:38:52 -0700

    TravelAroundWorld

---
+++ GetStarted/ConsoleApplication1/FillingJars.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication1 {
+    class FillingJars {
+        public static void MainFillingJars(string[] args) {
+            //Console.SetIn(new StreamReader(args[0]));
+            var numberOfJarsAndOperations = Console.ReadLine().Split(' ');
+            var numberOfJars = long.Parse(numberOfJarsAndOperations[0]);
+            var operationCount = long.Parse(numberOfJarsAndOperations[1]);
+            long[] index1 = new long[operationCount];
+            long[] index2 = new long[operationCount];
+            long[] candyCount = new long[operationCount];
+
+            for (int count = 0; count < operationCount; count++) {
+                var indexWithCandyCount = Console.ReadLine().Split(' ');
+                if (indexWithCandyCount.Length == 3) {
+                    index1[count] = long.Parse(indexWithCandyCount[0]);
+                    index2[count] = long.Parse(indexWithCandyCount[1]);
+                    candyCount[count] = long.Parse(indexWithCandyCount[2]);
+                }
+            }
+            long sum = 0;
+            for (int index = 0; index < operationCount; index++) {
+                sum += (index2[index] - index1[index] + 1) * candyCount[index];
+            }
+            Console.WriteLine(Math.Floor((decimal)sum / (decimal)numberOfJars));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GetStarted/ConsoleApplication1/TravelAroundWorld.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApplication1 {
+    class TravelAroundWorld {
+        public static void Main(string[] args) {
+            var cityAndCapacity = Console.ReadLine().Split(' ');
+            var numberOfCities = long.Parse(cityAndCapacity[0]);
+            var tankCapacity = long.Parse(cityAndCapacity[1]);
+            long[] fuelAvailableByCity = new long[numberOfCities];
+            long[] fuelRequiredBetweenCities = new long[numberOfCities];
+
+            var fuelAvailableByCityInput = Console.ReadLine().Split(' ');
+            var fuelRequiredBetweenCitiesInput = Console.ReadLine().Split(' ');
+            if (fuelAvailableByCityInput.Length == numberOfCities && fuelRequiredBetweenCitiesInput.Length == numberOfCities) {
+                for (long count = 0; count < fuelAvailableByCityInput.Length; count++) {
+                    fuelAvailableByCity[count] = long.Parse(fuelAvailableByCityInput[count]);
+                    fuelRequiredBetweenCities[count] = long.Parse(fuelRequiredBetweenCitiesInput[count]);
+                }
+                Console.WriteLine(CountCities(numberOfCities, tankCapacity, fuelAvailableByCity, fuelRequiredBetweenCities));
+                Console.ReadLine();
+            }
+        }
+
+        private static long CountCities(long numberOfCities, long tankCapacity, long[] fuelAvailableByCity, long[] fuelRequiredBetweenCities) {
+            long qualifiedCities = 0;
+            for (long startCityIndex = 0; startCityIndex < numberOfCities; startCityIndex++) {
+                // index + 1 through n - 1 and 0 through index
+                var currentCityIndex = startCityIndex;
+                var nextCityIndex = (currentCityIndex + 1) % (numberOfCities);
+                var currentTankCapacity = fuelAvailableByCity[currentCityIndex];
+                if (currentTankCapacity > tankCapacity) currentTankCapacity = tankCapacity;
+                for (long numberOfCitiesTravelled = 0; numberOfCitiesTravelled < numberOfCities; numberOfCitiesTravelled++) {
+                    currentTankCapacity -= fuelRequiredBetweenCities[currentCityIndex];
+                    if (currentTankCapacity < 0) break;
+                    if (nextCityIndex == startCityIndex) {
+                        qualifiedCities += 1;
+                        break;
+                    }
+                    currentCityIndex = nextCityIndex;
+                    nextCityIndex = (nextCityIndex + 1) % (numberOfCities);
+                    currentTankCapacity += fuelAvailableByCity[currentCityIndex];
+                    if (currentTankCapacity > tankCapacity) currentTankCapacity = tankCapacity;
+                }
+            }
+            return qualifiedCities;
+        }
+    }
+}
