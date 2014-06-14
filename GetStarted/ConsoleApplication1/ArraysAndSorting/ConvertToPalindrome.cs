099141cdd2d141c8824478af4e63ee6003509244
gbsandeep <gbsandeep@gmail.com>
2014-06-14 16:39:31 -0700

    ConvertToPalindrome

---
+++ GetStarted/ConsoleApplication1/ArraysAndSorting/ConvertToPalindrome.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApplication1 {
+    class ConvertToPalindrome {
+        public static void MainConvertToPalindrome() {
+            var n = long.Parse(Console.ReadLine());
+            var inputs = new string[n];
+            for(long i = 0; i < n; i++) {
+                inputs[i] = Console.ReadLine();
+            }
+            for(long i = 0; i < n; i++) {
+                Console.WriteLine(CostToMakePalindrome(inputs[i]));
+            }
+            Console.ReadLine();
+        }
+
+        private static long CostToMakePalindrome(string p) {
+            long result = 0;
+            for(int i = 0; i < p.Length / 2; i++) {
+                result += Math.Abs(p[i] - p[p.Length - i - 1]);
+            }
+            return result;
+        }
+    }
+}
